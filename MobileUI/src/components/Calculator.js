a7c764f75d0ee7ee1a250c80b48a4c0db2422ec9
Tiffany Ray <tiffanyraylee@gmail.com>
2022-02-21 16:38:15 -0700

    Adds radio buttons to select how to profit loss input changes affects which field option. Updates the usdSell input changes to update the coin sell price.

--- MobileUI/src/components/Calculator.js
+++ MobileUI/src/components/Calculator.js
@@ -1,6 +1,6 @@
 ﻿import * as React from 'react';
 import {View, Text, ScrollView, StyleSheet} from 'react-native';
-import {TextInput} from "react-native-paper";
+import {RadioButton, TextInput} from "react-native-paper";
 
 const isNum = (x) => {
   return !Number.isNaN(x) && !Number.isNaN(parseNum(x));
@@ -25,6 +25,7 @@ const reducer = (state, action) => {
   let usdBuy = valOrFalse(state.usdBuy);
   let usdSell = valOrFalse(state.usdSell);
   let profitLoss = valOrFalse(state.profitLoss);
+  let profitChanges = state.profitChanges;
     
   switch (action.type) {
     case actions.quantity:
@@ -108,7 +109,8 @@ const reducer = (state, action) => {
       if (usdBuy >= 0 && usdSell >= 0)
         profitLoss = usdSell - usdBuy;
       
-      // TODO: need to implement figuring out coinSellPrice when this var changes.
+      if (usdSell >= 0 && quantity >= 0)
+        coinSell = usdSell / quantity;
 
       return {
         ...state,
@@ -119,12 +121,37 @@ const reducer = (state, action) => {
     case actions.profitLoss:
       profitLoss = valOrFalse(action.value);
       
-      // TODO: need to implement switch / changing the price to gain the particular profit margin.
+      if (profitChanges === profitChangesOptions.sell) {
+        usdSell = profitLoss + usdBuy;
+
+        if (usdSell >= 0 && quantity >= 0)
+          coinSell = usdSell / quantity;
+        
+      } else if (profitChanges === profitChangesOptions.buy) {
+        usdBuy = usdSell - profitLoss;
+        
+        if (usdBuy >= 0 && quantity >= 0)
+          coinBuy = usdBuy / quantity;
+        
+      } else if (profitChanges === profitChangesOptions.quantity) {
+        quantity = profitLoss / (usdSell - usdBuy);
+      }
 
       return {
         ...state,
-        profitLoss: numRetVal(profitLoss)
+        profitLoss: numRetVal(profitLoss),
+        usdSell: numRetVal(usdSell),
+        usdBuy: numRetVal(usdBuy),
+        coinSell: numRetVal(coinSell),
+        coinBuy: numRetVal(coinBuy),
+        quantity: numRetVal(quantity)
       };
+      
+    case actions.profitChanges:
+      return {
+        ...state,
+        profitChanges: action.value
+      }
   }
 }
 
@@ -134,8 +161,15 @@ const actions = {
   coinSell: "coinSell",
   usdBuy: "usdBuy",
   usdSell: "usdSell",
-  profitLoss: "profitLoss"
+  profitLoss: "profitLoss",
+  profitChanges: "profitChanges"
 };
+
+const profitChangesOptions = {
+  buy: "buy",
+  sell: "sell",
+  quantity: "quantity"
+}
 
 export const Calculator = () => {
   const [state, dispatch] = React.useReducer(reducer, {
@@ -144,7 +178,8 @@ export const Calculator = () => {
     usdBuy: "0",
     coinSell: "0",
     usdSell: "0",
-    profitLoss: "0"
+    profitLoss: "0",
+    profitChanges: profitChangesOptions.sell
   });
   
   return (
@@ -194,6 +229,24 @@ export const Calculator = () => {
         value={state.profitLoss}
         style={styles.input}
         keyboardType='numeric'
+      />
+      <Text>Sell</Text>
+      <RadioButton
+        value={profitChangesOptions.sell}
+        status={state.profitChanges === profitChangesOptions.sell ? 'checked' : 'unchecked'}
+        onPress={() => dispatch({ type: actions.profitChanges, value: profitChangesOptions.sell })}
+      />
+      <Text>Quantity</Text>
+      <RadioButton
+        value={profitChangesOptions.quantity}
+        status={state.profitChanges === profitChangesOptions.quantity ? 'checked' : 'unchecked'}
+        onPress={() => dispatch({ type: actions.profitChanges, value: profitChangesOptions.quantity })}
+      />
+      <Text>Buy</Text>
+      <RadioButton
+        value={profitChangesOptions.buy}
+        status={state.profitChanges === profitChangesOptions.buy ? 'checked' : 'unchecked'}
+        onPress={() => dispatch({ type: actions.profitChanges, value: profitChangesOptions.buy })}
       />
     </ScrollView>
   )
