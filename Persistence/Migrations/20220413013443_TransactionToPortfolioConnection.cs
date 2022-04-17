8dabc2e4bfd38d323377baabb0d0c1ac6563e542
Tiffany Ray <tiffanyraylee@gmail.com>
2022-04-17 08:22:28 -0600

    Transaction migrtion files

---
+++ Persistence/Migrations/20220413013443_TransactionToPortfolioConnection.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class TransactionToPortfolioConnection : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PortfolioId",
+                table: "Transactions",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_PortfolioId",
+                table: "Transactions",
+                column: "PortfolioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Portfolios_PortfolioId",
+                table: "Transactions",
+                column: "PortfolioId",
+                principalTable: "Portfolios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Portfolios_PortfolioId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_PortfolioId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "PortfolioId",
+                table: "Transactions");
+        }
+    }
+}
